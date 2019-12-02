2681ff87d4cf9234bacaf55ab2c1892c92a722fe
Csondika <csondikaa@gmail.com>
2019-12-02 08:52:09 +0100

    Implement constructor with 2 arguments in PaperGarbage.cs

--- waste-recycling/PaperGarbage.cs
+++ waste-recycling/PaperGarbage.cs
@@ -2,5 +2,10 @@ namespace WasteRecycling
 {
     public class PaperGarbage : Garbage
     {
+        public bool Squeezed { get; set; }
+        public PaperGarbage(string garbageName, bool squeezed) : base(garbageName)
+        {
+            this.Squeezed = squeezed;
+        }
     }
 }
