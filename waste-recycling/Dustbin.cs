30bd29a822cfb722252c9aca6ea273dd2b4b8e91
Csondika <csondikaa@gmail.com>
2019-12-02 09:24:34 +0100

    Implement properties and constructor in Dustbin.cs

--- waste-recycling/Dustbin.cs
+++ waste-recycling/Dustbin.cs
@@ -2,5 +2,17 @@ namespace WasteRecycling
 {
     public class Dustbin
     {
+        public string Color { get; set; }
+        public PaperGarbage[] PaperContent { get; set; }
+        public PlasticGarbage[] PlasticContent { get; set; }
+        public Garbage[] HouseWasteContent { get; set; }
+
+        public Dustbin(string color)
+        {
+            this.Color = color;
+            PaperContent = new PaperGarbage[100];
+            PlasticContent = new PlasticGarbage[100];
+            HouseWasteContent = new Garbage[100];
+        }
     }
 }
