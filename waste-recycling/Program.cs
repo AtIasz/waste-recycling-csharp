fc588870eeb9ea5656609fcb8ad0c307b5288e46
Csondika <csondikaa@gmail.com>
2019-12-02 10:25:16 +0100

    Clear Program.cs

--- waste-recycling/Program.cs
+++ waste-recycling/Program.cs
@@ -1,19 +1,12 @@
+using System;
+
 namespace WasteRecycling
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Dustbin dustbin = new Dustbin("Red");
-
-            PlasticGarbage plasticGarbage = new PlasticGarbage("plastic", true);
-            dustbin.ThrowOutGarbage(plasticGarbage);
-            PaperGarbage paperGarbage = new PaperGarbage("paper", true);
-            dustbin.ThrowOutGarbage(paperGarbage);
-            Garbage garbage = new Garbage("house");
-            dustbin.ThrowOutGarbage(garbage);
-
-            dustbin.DisplayContents();
+            
         }
     }
 }
